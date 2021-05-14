42bd13133f6710ffa7f55756db87a66ded51b9c2
Jonathan Dushoff <dushoff@mcmaster.ca>
2021-05-13 23:45:47 -0400

    Post a blog

--- forecasts/fd.pl
+++ forecasts/fd.pl
@@ -1,10 +1,16 @@
 use strict;
 use 5.10.0;
 
-my $target = shift @ARGV;
+my $hf = "    includes:\n      before_body: header.html\n      after_body: footer.html\n---";
 
 while(<>){
-	chomp;
-	s|\([\w.-]*_files/|(${target}_files/|;
-	say;
+	last if $. > 2 && /---/;
+	print;
 }
+
+say $hf;
+
+while(<>){
+	print;
+}
+
